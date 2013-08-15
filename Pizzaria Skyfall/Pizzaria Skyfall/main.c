ef7dcd846061fcc226741b968ef2a30011aef908
Erick Sasse Filho <esfilho@gmail.com>
2013-08-15 20:59:31 -0300

    Commit inicial, arquivo main.c

---
+++ Pizzaria Skyfall/Pizzaria Skyfall/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Pizzaria Skyfall
+//
+//  Created by Erick Sasse Filho on 15/08/13.
+//  Copyright (c) 2013 Erick Sasse Filho. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
